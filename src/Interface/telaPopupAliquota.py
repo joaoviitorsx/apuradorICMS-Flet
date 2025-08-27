1398df35c1b31c8ee674f1ff9a50bed028d26e12
João Vitor Silva <joaovitorssilva007@gmail.com>
2025-08-27 18:04:54 -0300

    Interface

--- src/Interface/telaPopupAliquota.py
+++ src/Interface/telaPopupAliquota.py
@@ -1,5 +1,6 @@
 import flet as ft
-from src.Components.Aliquota.aliquotaDialog import abrirDialogoAliquotas
+from src.Components.PoupAliquota.aliquotaDialog import abrirDialogoAliquotas
 
 def mostrarTelaPoupAliquota(page: ft.Page, empresa_id: int):
     abrirDialogoAliquotas(page, empresa_id)
+
